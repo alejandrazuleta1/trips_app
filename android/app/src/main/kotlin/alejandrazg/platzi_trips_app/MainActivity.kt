9fd7e5780e47a0c65391944437a327f095c72c97
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2021-05-19 00:10:10 -0500

    Agregados widgets sin estado iniciales

--- android/app/src/main/kotlin/alejandrazg/platzi_trips_app/MainActivity.kt
+++ android/app/src/main/kotlin/alejandrazg/platzi_trips_app/MainActivity.kt
@@ -1,6 +1,16 @@
 package alejandrazg.platzi_trips_app
 
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        this.flutterEngine?.let { GeneratedPluginRegistrant.registerWith(it) }
+        this.getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT)
+    }
 }
