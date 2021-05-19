690f5bbaa536f29b2b3437b99ae7168f6ffd3d4e
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2021-05-19 12:22:53 -0500

    Adicion del widget stateful boton flotante en la card

---
+++ lib/floating_action_button_green.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _FloatingActionButtonGreen();
+  }
+}
+
+class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen> {
+  IconData iconData = Icons.favorite_border;
+  void onPressedFav(){
+    setState(() => iconData = iconData == Icons.favorite_border ? Icons.favorite : Icons.favorite_border);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF11DA53),
+      mini: true,
+      tooltip: "Fav",
+      onPressed: onPressedFav,
+      child: Icon(
+          iconData
+      ),
+    );
+  }
+
+}
