11846b14854c7adc97de976f2dfaacf9ca56729c
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2021-05-19 21:14:42 -0500

    Agregado header en profile view

---
+++ lib/profile_header.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/user_info.dart';
+import 'button_bar.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = Text(
+      'Profile',
+      style: TextStyle(
+        fontFamily: 'Lato',
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 30.0
+      ),
+    );
+
+    return Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+        top: 50.0
+      ),
+      child: Column(
+        children: <Widget> [
+          Row(
+            children: [
+              title
+            ],
+          ),
+          UserInfo('assets/img/avatar.png', 'Alejandra Zuleta','aleja@gmail.com'),
+          ButtonsBar()
+        ],
+      ),
+    );
+  }
+
+}
--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/profile_backgroud.dart';
+import 'package:platzi_trips_app/profile_header.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build profile
-    return Container(
-      color: Colors.indigo,
+    return Stack(
+      children: [
+        ProfileBackground(),
+        ListView(
+          children: [
+            ProfileHeader(),
+          ],
+        )
+      ],
     );
   }
 
